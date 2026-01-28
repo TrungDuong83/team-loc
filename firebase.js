1f5ac4768b723ad1ec2c5b4add0841f104cb16bd
TrungDuong83 <131349815+TrungDuong83@users.noreply.github.com>
2026-01-28 23:04:07 +0700

    Update firebase.js
    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
    import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
    
    const firebaseConfig = {
      apiKey: "AIzaSyA-J9AVShiJep8aWHkIAgjXk1nQfxvc3tI",
      authDomain: "team-loc.firebaseapp.com",
      databaseURL: "https://team-loc-default-rtdb.asia-southeast1.firebasedatabase.app",
      projectId: "team-loc",
      storageBucket: "team-loc.firebasestorage.app",
      messagingSenderId: "791200789261",
      appId: "1:791200789261:web:f819950d08280219657644"
    };
    
    const app = initializeApp(firebaseConfig);
    export const db = getDatabase(app);

--- firebase.js
+++ firebase.js
@@ -1,26 +1,22 @@
 <script type="module">
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyA-J9AVShiJep8aWHkIAgjXk1nQfxvc3tI",
-    authDomain: "team-loc.firebaseapp.com",
-    databaseURL: "https://team-loc-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "team-loc",
-    storageBucket: "team-loc.firebasestorage.app",
-    messagingSenderId: "791200789261",
-    appId: "1:791200789261:web:f819950d08280219657644",
-    measurementId: "G-YVXSTZWKL9"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyA-J9AVShiJep8aWHkIAgjXk1nQfxvc3tI",
+  authDomain: "team-loc.firebaseapp.com",
+  databaseURL: "https://team-loc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "team-loc",
+  storageBucket: "team-loc.firebasestorage.app",
+  messagingSenderId: "791200789261",
+  appId: "1:791200789261:web:f819950d08280219657644"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  export const db = getDatabase(app);
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+
 
 </script>
+
 
